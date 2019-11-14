06c6e8ea0b56ce2159c36145b34c8e0c48096972
aparkening <aaron.parkening@gmail.com>
2019-11-14 17:23:40 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arrayOfObjects){
+  let win = arrayOfObjects.find(obj => obj.result === "W")
+  return !!win ? win.year : undefined
+}

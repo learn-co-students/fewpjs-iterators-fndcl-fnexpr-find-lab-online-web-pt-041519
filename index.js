9e72a4a2ad24041c53b015c7aac2bf2b028098f5
Michael Chen <michaelchengaming@gmail.com>
2019-11-18 11:22:19 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(record){
+  let w = record.find(win => {return win.result === 'W'});
+  if (w){
+    return w.year;
+  } else {
+    return undefined;
+  };
+};

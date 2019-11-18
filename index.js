a853ccf8a4068f7964c0685bfa3e107dc1f809f0
gzavaglia <gio.zavaglia@gmail.com>
2019-11-17 18:17:52 -0600

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+import { finished } from "stream"
+import { type } from "os";
+
 const testVar = {}
 
 function testFunc() {
   return "hi"
+}
+
+
+function superbowlWin(record)
+{
+  const result = record.find( ({ result }) => result === 'W' );
+  if (typeof result === 'undefined')
+  {
+    return undefined
+  } else {
+  return result.year
+  }
 }

8d09c742eef6ccec7ee0f306475b8572aaea7e1d
lanibon <nguyenlan13@gmail.com>
2019-11-18 18:34:27 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+
+superbowlWin = (record) => {
+    let outcome = record.find(record => record.result === "W");
+ if (outcome){
+     return outcome.year;
+ }
+ else {
+     return undefined;
+ }
+}
+ 
+ 
+//     if (outcome){
+//    return record.year
+//  }
+//  else (outcome === false){
+//     return undefined
+// }
+// }
+

b7acbd1b1d23351bb85bfb5424c6b1ff416ce3de
earthctzn <earthctzn1@gmail.com>
2019-11-23 15:59:14 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const superbowlWin = (arr) => {
+  let isWin = arr.find(year => year.result === "W")
+  console.log(arr.year)
+  return isWin ? isWin.year : undefined
+}

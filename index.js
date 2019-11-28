30fc30b664c60858a16a42aaa833204f7efa9e9b
Cody <cody.frank30@gmail.com>
2019-11-28 18:23:45 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let object = array.find( o => o.result === "W")
+  if(object){
+    return object.year
+  }else{
+    return undefined
+  }
+}

78c91e496c36959c954537b02956c3201df3aacc
Micah Shute <micah.shute@gmail.com>
2019-11-29 17:39:32 -0800

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const testVar = {}
+// const testVar = {}
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+//   return "hi"
+// }
+
+
+const superbowlWin = (arr) => {
+  const res = arr.find(obj => obj.result === "W" )
+  if(res){ return res.year }
 }
